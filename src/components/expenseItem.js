a00db9cae6f64b0d295b4dbe169a1d02ea6d614f
Jerry Hou <chengrui.hou@duke.edu>
2021-12-11 01:01:49 -0500

    initial commit

---
+++ src/components/expenseItem.js
@@ -0,0 +1,15 @@
+import "./expenseItem.css";
+import ExpenseDate from "./expenseDate";
+import Card from "./card";
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+export default ExpenseItem;
